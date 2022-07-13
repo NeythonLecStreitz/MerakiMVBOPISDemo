54d2257c65e0dcd76bf23e41b46f636729ca368b
NeythonLecStreitz <70167258+NeythonLecStreitz@users.noreply.github.com>
2022-07-13 14:59:38 -0500

    Update rds_config.py

--- rds_config.py
+++ rds_config.py
@@ -1,5 +1,5 @@
 #config file containing credentials for RDS MySQL instance
 
-db_username = 'nlecstreitz'
-db_password = 'Soccer24'
-db_name = 'customer_db'
+db_username = ''
+db_password = ''
+db_name = ''
